5156035d9c9f10f2a5e926bb1ce4874cd00d93af
leonardsmalls <corey@ahappylittledesignstudio.com>
2017-06-05 03:55:21 -0500

    commit

---
+++ app/data/bands.js
@@ -0,0 +1,25 @@
+var bandsArray = [
+  {
+    bandName: "Grateful Dead",
+    bandRating: [5,2,5,2,5,2,5,2,5,2]
+  },
+  {
+    bandName: "Nirvana",
+    bandRating: [4,3,4,3,2,3,4,3,1,4]
+  },
+  {
+    bandName: "Notorious B.I.G",
+    bandRating: [3,1,1,3,1,3,1,3,1,3]
+  },
+  {
+    bandName: "Willie Nelson",
+    bandRating: [2,1,3,4,5,5,4,3,2,2]
+  },
+  {
+    bandName: "Billie Joel",
+    bandRating: [1,2,4,1,1,5,5,1,1,1]
+  }
+]
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = bandsArray
